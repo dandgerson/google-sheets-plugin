8b23046aca4743510507195787e977c1bcc655ee
Dmitry G. Anderson <dandgerson@gmail.com>
2023-08-12 19:09:17 +0300

    chore: drop unneeded

--- src/client/dialog-demo-mui/components/FormInput.tsx
+++ src/client/dialog-demo-mui/components/FormInput.tsx
@@ -23,9 +23,7 @@ const FormInput = ({ submitNewSheet }: FormInputProps) => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-    >
+    <form onSubmit={handleSubmit}>
       <Grid container spacing={2}>
         <Grid item xs={10}>
           <TextField
@@ -53,7 +51,7 @@ const FormInput = ({ submitNewSheet }: FormInputProps) => {
         </Grid>
       </Grid>
       <Typography variant="caption" gutterBottom>
-        Enter the name for your new sheet.
+        Enter the name for your new sheet. 123
       </Typography>
       <br />
       <Typography variant="caption" gutterBottom sx={{ fontStyle: 'italic' }}>
